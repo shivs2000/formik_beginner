a9b27f6d06f55a2edb2291f19524de80d17dbb44
shivs2000 <singhshivam0666@gmail.com>
2022-09-04 19:51:52 +0530

    added formik instead of useFormik

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -16,57 +16,56 @@ const validationSchema = Yup.object({
   channel: Yup.string().required("Required!!"),
 });
 const YouTubeForm = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-    //validate,
-  });
-
   //console.log("form values", formik.values);
   //console.log("form errors", formik.errors);
   console.log("visited fields", formik.touched);
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="form-control">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div>{formik.errors.name}</div>
-          ) : null}
-        </div>
-        <div className="form-control">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
-        </div>
-        <div className="form-control">
-          <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...formik.getFieldProps("channel")}
-          />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div>{formik.errors.channel}</div>
-          ) : null}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <form onSubmit={formik.handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              {...formik.getFieldProps("name")}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <div>{formik.errors.name}</div>
+            ) : null}
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              {...formik.getFieldProps("email")}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div>{formik.errors.email}</div>
+            ) : null}
+          </div>
+          <div className="form-control">
+            <label htmlFor="channel">Channel</label>
+            <input
+              type="text"
+              id="channel"
+              name="channel"
+              {...formik.getFieldProps("channel")}
+            />
+            {formik.touched.channel && formik.errors.channel ? (
+              <div>{formik.errors.channel}</div>
+            ) : null}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </Formik>
     </div>
   );
 };
